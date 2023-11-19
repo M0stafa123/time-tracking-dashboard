40f87c096bd42c424045891c1bda5e276fd5f5f5
M0stafa123 <jodat.ahmed135@gmail.com>
2023-11-19 02:29:49 +0200

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bluePrimary: "hsl(246, 80%, 60%)",
+        work: "hsl(15, 100%, 70%)",
+        play: "hsl(195, 74%, 62%)",
+        study: "hsl(348, 100%, 68%)",
+        exercise: "hsl(145, 58%, 55%)",
+        social: "hsl(264, 64%, 52%)",
+        selfCare: "hsl(43, 84%, 65%)",
+        VdarkBlue: "hsl(226, 43%, 10%)",
+        darkBlue: "hsl(235, 46%, 20%)",
+        desaturatedBlue: "hsl(235, 45%, 40%)",
+        paleBlue: "hsl(236, 100%, 87%)",
+      },
+      fontFamily: {
+        Rubik: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
